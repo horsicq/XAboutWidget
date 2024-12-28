19ee920bfcf43895bd5edaed4821e134e3573a91
horsicq <horsicq@gmail.com>
2024-12-28 01:12:07 +0100

    Update file(s): 2024-12-28

--- xaboutwidget.cmake
+++ xaboutwidget.cmake
@@ -7,7 +7,6 @@ endif()
 
 set(XABOUTWIDGET_SOURCES
     ${XABOUTWIDGET_SOURCES}
-    ${XSHORTCUTS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xaboutwidget.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xaboutwidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xaboutwidget.ui
